5628aa24faba9472991e1b1bdd36257ba6808d20
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2025-02-16 06:43:31 -0600

    Initial commit

---
+++ src/conway.hpp
@@ -0,0 +1,118 @@
+
+#include <stdlib.h>
+#include <Arduino.h>
+#define WIDTH 20
+#define HEIGHT 20
+
+int resize(int x, int max_value)
+{
+    if (x < 0)
+    {
+        return max_value - (abs(x) % max_value);
+    }
+    if (x >= max_value)
+    {
+        return x % max_value;
+    }
+    return x;
+}
+class Conway
+{
+
+public:
+    bool *map;
+    bool updating=false;
+    // #  or 0
+    Conway(char *initialMap)
+    {
+        this->map = new bool[WIDTH * HEIGHT];
+        // so aqui tenemos esta parte donde copia una base :D
+        for (int i = 0; i < WIDTH * HEIGHT; i++)
+        {
+            this->map[i] = initialMap[i] == '#';
+        }
+    }
+    //** esto va a ser usado desde el servidor :D */
+    void update_slice(int x, int y, bool is_alive)
+    {
+        int nx = resize(x, WIDTH);
+        int ny = resize(y, HEIGHT);
+        map[ny * WIDTH + nx] = is_alive;
+    }
+    char *map_to_char()
+    {
+        char *char_map = new char[WIDTH * HEIGHT];
+        for (int i = 0; i < WIDTH * HEIGHT; i++)
+        {
+            char_map[i] = this->map[i] ? '#' : ' ';
+        }
+        return char_map;
+    }
+    int check_neightbours(int x, int y)
+    {
+        int nx = resize(x, WIDTH);
+        int ny = resize(y, HEIGHT);
+        int count = 0;
+        for (int ly = -1; ly <= 1; ly++)
+        {
+            for (int lx = -1; lx <= 1; lx++)
+            {
+                if (lx == 0 && ly == 0)
+                    continue;
+
+                if (this->map[resize(ny + ly, HEIGHT) * WIDTH + resize(nx + lx, WIDTH)])
+                    count++;
+            }
+        }
+        return count;
+    }
+    void update_map()
+    {
+        bool *new_map=new bool[WIDTH*HEIGHT];
+        for (int y = 0; y < HEIGHT; y++)
+        {
+            for (int x = 0; x < WIDTH; x++)
+            {
+
+                int neighbours = this->check_neightbours(x, y);
+                if ((this->map[y * WIDTH + x] && (neighbours == 2)) || neighbours == 3)
+                {
+                    new_map[y * WIDTH + x] = true;
+                    continue;
+                }
+
+                new_map[y * WIDTH + x] = false;
+            }
+        }
+        this->map=new_map;
+    }
+};
+
+char *returnMapChar()
+{
+    String initialMap = 
+                  "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "0000000#000000000000";
+    initialMap += "00000000#00000000000";
+    initialMap += "000000###00000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+    initialMap += "00000000000000000000";
+
+    char *initialMapChar = new char[WIDTH * HEIGHT];
+    memcpy(initialMapChar, initialMap.c_str(), WIDTH * HEIGHT);
+    return initialMapChar;
+}
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include "conway.hpp"
+// put function declarations here:
+int myFunction(int, int);
+Conway conway(returnMapChar());
+int fps=1000;
+long lastTime=0;
+int f=0;
+
+// now i should work on the way i render stuff 
+void setup()
+{
+  
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  // so ahora solo es tiempo de que me ponga a hacer el juego de conway
+  Serial.println("");
+}
+
+void loop()
+{
+  // how i can get the current time?
+  long currentTime = millis();
+  long deltaTime = currentTime - lastTime;
+  if(f<10){
+    f++;
+    Serial.println("");
+    return;
+  }
+  if (deltaTime >= fps)
+  {
+    lastTime = currentTime;
+    char* char_map = conway.map_to_char();
+    Serial.println("-------------------");
+    for (int y = 0; y < HEIGHT; y++){
+      for (int x = 0; x < WIDTH; x++){
+        Serial.print(char_map[y*WIDTH+x]);
+      }
+      Serial.println();
+    }
+    Serial.println();
+    conway.update_map();
+
+  }
+  // put your main code here, to run repeatedly:
+}
+
