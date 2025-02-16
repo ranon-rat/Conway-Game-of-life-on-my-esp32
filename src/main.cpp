cbdb91d86736c01f184347e45accd50bd24ecf89
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2025-02-16 07:14:34 -0600

    unnecessary shit

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,18 @@
 #include <Arduino.h>
 #include <wifiConfig.h>
 #include "conway.hpp"
-// put function declarations here:
-int myFunction(int, int);
 Conway conway(returnMapChar());
-int fps=1000;
+int fps=1000/1;
 long lastTime=0;
-int f=0;
-
-// now i should work on the way i render stuff 
 void setup()
 {
-  
   Serial.begin(115200);
-  // put your setup code here, to run once:
-  // so ahora solo es tiempo de que me ponga a hacer el juego de conway
-  Serial.println("");
 }
 
 void loop()
 {
-  // how i can get the current time?
   long currentTime = millis();
   long deltaTime = currentTime - lastTime;
-  if(f<10){
-    f++;
-    Serial.println("");
-    return;
-  }
   if (deltaTime >= fps)
   {
     lastTime = currentTime;
@@ -43,6 +28,5 @@ void loop()
     conway.update_map();
 
   }
-  // put your main code here, to run repeatedly:
 }
 
